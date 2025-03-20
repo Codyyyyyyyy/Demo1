cb601fc2983d624b3453630cc8a26868ba4a16a0
Cody <415711805@qq.com>
2025-03-20 13:18:13 +0800

    Daily Update

---
+++ push.py
@@ -0,0 +1,15 @@
+from git import Git, Repo
+
+local_repo_path = r'D:\AirtestProject\automation.air_mumu'
+remote_repo_url = 'git@github.com:Codyyyyyyyy/Demo1.git'
+try:
+    # 打开本地仓库
+    repo = Repo(local_repo_path)
+    repo.git.add(all=True)
+    repo.index.commit('Daily Update')
+    # 使用Git对象执行推送操作
+    g = Git(repo.working_dir)
+    g.push(remote_repo_url)
+    print('push success')
+except Exception as e:
+    print(f'Something goes wrong:{e}')
